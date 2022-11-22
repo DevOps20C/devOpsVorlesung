ced08c4d402666fbb63382b5b38b823df47e7708
Yannis Plaschko <yannis.plaschko@breuninger.de>
2022-11-22 11:12:22 +0100

    introduce terraform

--- devops/main.tf
+++ devops/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_service_plan" "app-service-plan" {
 }
 
 resource "azurerm_linux_web_app" "app" {
-    name = "app-devops" # Name - muss eindeutig sein
+    name = "app-devops"
     resource_group_name = azurerm_resource_group.rg-devops.name
     location = azurerm_resource_group.rg-devops.location
     service_plan_id = azurerm_service_plan.app-service-plan.id
