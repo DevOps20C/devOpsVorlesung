1a3fa50d3b4158065bf1dfed29c61aea85324b3f
Kevin Pauer <kevinpauer@t-online.de>
2022-12-09 21:00:30 +0100

    Reformat terraform file; Rename ressource group; Initialized terraform

--- devops/main.tf
+++ devops/main.tf
@@ -1,41 +1,41 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~> 3.0.2"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
-    required_version = ">= 1.0.0"
+  }
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "rg-devops" {
-    name = "rg-devops"
-    location = var.region
+  name     = "rg-devops"
+  location = var.region
 }
 
 resource "azurerm_service_plan" "app-service-plan" {
-    name = "devOps"
-    resource_group_name = azurerm_resource_group.rg-devops.name
-    location = azurerm_resource_group.rg-devops.location
-    sku_name = "F1"
-    os_type = "Linux"
+  name                = "devOps"
+  resource_group_name = azurerm_resource_group.rg-devops.name
+  location            = azurerm_resource_group.rg-devops.location
+  sku_name            = "F1"
+  os_type             = "Linux"
 }
 
 resource "azurerm_linux_web_app" "app" {
-    name = "app-devops"
-    resource_group_name = azurerm_resource_group.rg-devops.name
-    location = azurerm_resource_group.rg-devops.location
-    service_plan_id = azurerm_service_plan.app-service-plan.id
+  name                = "app-devops-dhbw"
+  resource_group_name = azurerm_resource_group.rg-devops.name
+  location            = azurerm_resource_group.rg-devops.location
+  service_plan_id     = azurerm_service_plan.app-service-plan.id
 
-    site_config {
-        always_on = false
-        application_stack {
-            docker_image = ""
-            docker_image_tag = "latest"
-        }
+  site_config {
+    always_on = false
+    application_stack {
+      docker_image     = "dhbwdevops"
+      docker_image_tag = "latest"
     }
+  }
 }
