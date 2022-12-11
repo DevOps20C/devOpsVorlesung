ab0a1ce6ef544e03ca9ed52ac472f14fa5fc2e7f
Kevin Pauer <kevinpauer@t-online.de>
2022-12-11 22:33:00 +0100

    Update to use private registry

--- devops/main.tf
+++ devops/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "rg-devops" {
 }
 
 resource "azurerm_service_plan" "app-service-plan" {
-  name                = "devOps"
+  name                = "${azurerm_resource_group.rg-devops.name}-plan" 
   resource_group_name = azurerm_resource_group.rg-devops.name
   location            = azurerm_resource_group.rg-devops.location
   sku_name            = "F1"
@@ -30,6 +30,15 @@ resource "azurerm_linux_web_app" "app" {
   resource_group_name = azurerm_resource_group.rg-devops.name
   location            = azurerm_resource_group.rg-devops.location
   service_plan_id     = azurerm_service_plan.app-service-plan.id
+
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+
+    # Settings for private Container Registires  
+    DOCKER_REGISTRY_SERVER_URL      = "https://public-registry.amtmann.de/"
+    DOCKER_REGISTRY_SERVER_USERNAME = "publicregistry"
+    DOCKER_REGISTRY_SERVER_PASSWORD = "WBJWdiPhnDsQUB3iKFAwRAYxV"
+  }
 
   site_config {
     always_on = false
